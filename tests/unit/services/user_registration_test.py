70893a0f59aaf1f41ef495994a51f00a71699cf4
Ivan Novozhilov <petyk98@gmail.com>
2025-02-27 07:22:51 +0700

    Feature: Add UserRegistrationService tests.

---
+++ tests/unit/services/user_registration_test.py
@@ -0,0 +1,55 @@
+import pytest
+
+from src.domain.entities.user import UserEntity
+from src.domain.exceptions.user_registration import (
+    NicknameAlreadyExistException,
+    PasswordConstraintException,
+)
+from src.domain.repositories.interfaces import IUsersRepository
+from src.domain.services.user_registration import UserRegistrationService
+
+
+async def test_register_user_success(
+    user_registration_service: UserRegistrationService,
+    users_repository: IUsersRepository,
+    user: UserEntity,
+):
+    await user_registration_service.register_user(
+        nickname=user.nickname,
+        plain_password=user.hashed_password,
+    )
+
+    assert await users_repository.get_by_nickname(user.nickname)
+
+
+async def test_register_user_by_exist_nickname_exception(
+    user_registration_service: UserRegistrationService,
+    users_repository: IUsersRepository,
+    user: UserEntity,
+):
+    await users_repository.create(user)
+
+    with pytest.raises(NicknameAlreadyExistException):
+        await user_registration_service.register_user(
+            nickname=user.nickname,
+            plain_password=user.hashed_password,
+        )
+
+
+@pytest.mark.parametrize(
+    "bad_password",
+    [
+        "weak",
+        "too_long" * 42,
+    ],
+)
+async def test_register_user_password_constraint_exception(
+    user_registration_service: UserRegistrationService,
+    user: UserEntity,
+    bad_password: str,
+):
+    with pytest.raises(PasswordConstraintException):
+        await user_registration_service.register_user(
+            nickname=user.nickname,
+            plain_password=bad_password,
+        )
