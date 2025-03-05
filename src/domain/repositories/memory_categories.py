3efd1dff28701172a7fed9a7d43288629c90c868
Ivan Novozhilov <petyk98@gmail.com>
2025-03-05 07:56:15 +0700

    Fix: Change MemoryArticlesRepository name of field _storage to storage.

--- src/domain/repositories/memory_categories.py
+++ src/domain/repositories/memory_categories.py
@@ -6,16 +6,16 @@ from src.domain.repositories.interfaces import ICategoriesRepository
 
 @dataclass
 class MemoryCategoriesRepository(ICategoriesRepository):
-    _storage: set[CategoryEntity] = field(default_factory=set)
+    storage: set[CategoryEntity] = field(default_factory=set)
 
     async def create_many(self, categories: set[CategoryEntity]) -> None:
         for category in categories:
-            if category in self._storage:
+            if category in self.storage:
                 continue
-            self._storage.add(category)
+            self.storage.add(category)
 
     async def get_by_name(self, name: str) -> CategoryEntity | None:
         return next(
-            (category for category in self._storage if category == name),
+            (category for category in self.storage if category == name),
             None,
         )
