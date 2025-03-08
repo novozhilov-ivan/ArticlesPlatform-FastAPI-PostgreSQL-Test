d52897d9569d5fda98417d81edf504d205ea9740
Ivan Novozhilov <petyk98@gmail.com>
2025-03-08 11:48:42 +0700

    Refactoring: fix create oid uuid4 in fixture user.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-from uuid import uuid4
-
 import pytest
 
 from src.domain.entities.article import ArticleEntity
@@ -11,7 +9,6 @@ from src.domain.entities.user import UserEntity
 @pytest.fixture(scope="session")
 def user() -> UserEntity:
     return UserEntity(
-        oid=uuid4(),
         nickname="nickname",
         password="plain_password",
     )
