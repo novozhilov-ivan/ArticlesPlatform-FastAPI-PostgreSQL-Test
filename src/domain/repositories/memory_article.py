38161c5b8f5935e77d649d612b495de0552df350
Ivan Novozhilov <petyk98@gmail.com>
2025-03-09 11:04:09 +0700

    Feature: add dependencies in MemoryArticlesRepository.

--- src/domain/repositories/memory_article.py
+++ src/domain/repositories/memory_article.py
@@ -2,7 +2,10 @@ from dataclasses import dataclass, field
 from uuid import UUID
 
 from src.domain.entities.article import ArticleEntity
-from src.domain.repositories.interfaces import IArticlesRepository
+from src.domain.repositories.interfaces import (
+    IArticlesRepository,
+    ICategoriesRepository,
+)
 from src.domain.repositories.memory_association import (
     MemoryArticleCategoryAssociationsRepository,
 )
@@ -10,24 +13,46 @@ from src.domain.repositories.memory_association import (
 
 @dataclass(kw_only=True)
 class MemoryArticlesRepository(IArticlesRepository):
-    storage: set[ArticleEntity] = field(default_factory=set)
+    categories_repository: ICategoriesRepository
     associations_repository: MemoryArticleCategoryAssociationsRepository
+    storage: set[ArticleEntity] = field(default_factory=set)
 
     async def create(self, article: ArticleEntity) -> None:
         if article in self.storage:
             return
         self.storage.add(article)
+        await self.categories_repository.create_many(article.categories)
+        await self.associations_repository.create_many(article)
+
+    async def load_categories(self, article: ArticleEntity) -> ArticleEntity:
+        categories_names = (
+            await self.associations_repository.get_categories_names_by_article_oid(
+                article.oid
+            )
+        )
+        for category_name in categories_names:
+            category = await self.categories_repository.get_by_name(category_name)
+            if category:
+                article.categories.add(category)
+
+        return article
 
     async def get_by_oid(self, oid: UUID) -> ArticleEntity | None:
-        return next((article for article in self.storage if article == oid), None)
+        try:
+            article = next(article for article in self.storage if article == oid)
+        except StopIteration:
+            return None
+
+        return await self.load_categories(article)
 
     async def list(self) -> set[ArticleEntity]:
         return self.storage
 
     async def delete_by_oid(self, oid: UUID) -> None:
-        article = next(
-            (article for article in self.storage if article.oid == oid),
-            None,
-        )
-        if article:
-            self.storage.remove(article)
+        try:
+            article = next(article for article in self.storage if article.oid == oid)
+        except StopIteration:
+            return
+
+        self.storage.remove(article)
+        await self.associations_repository.remove_by_article_oid(oid)
