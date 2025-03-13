183a0d3fae24efa1ff353810cd57c4258104b3a0
Ivan Novozhilov <petyk98@gmail.com>
2025-03-13 07:23:21 +0700

    Feature: Add methods delete_by_oid, get_by_oid in ArticleService and tests.

--- src/domain/services/article.py
+++ src/domain/services/article.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from uuid import UUID
 
 from src.domain.entities.article import ArticleEntity
 from src.domain.repositories.interfaces import (
@@ -15,3 +16,9 @@ class ArticleService:
 
     async def get_list(self) -> set[ArticleEntity]:
         return await self.articles_repository.list()
+
+    async def delete_by_oid(self, oid: UUID) -> None:
+        await self.articles_repository.delete_by_oid(oid)
+
+    async def get_by_oid(self, oid: UUID) -> ArticleEntity | None:
+        return await self.articles_repository.get_by_oid(oid)
--- tests/unit/services/article_test.py
+++ tests/unit/services/article_test.py
@@ -23,3 +23,36 @@ async def test_get_list_with_article(
     articles_list = await article_service.get_list()
 
     assert articles_list == articles_repository.storage
+
+
+async def test_delete_by_oid(
+    article: ArticleEntity,
+    article_service: ArticleService,
+    articles_repository: MemoryArticlesRepository,
+):
+    articles_repository.storage = {article}
+
+    await article_service.delete_by_oid(article.oid)
+
+    assert not articles_repository.storage
+
+
+async def test_get_by_oid_non_exist_article(
+    article: ArticleEntity,
+    article_service: ArticleService,
+):
+    article_from_service = await article_service.get_by_oid(article.oid)
+
+    assert article_from_service is None
+
+
+async def test_get_by_oid_exist_article(
+    article: ArticleEntity,
+    article_service: ArticleService,
+    articles_repository: MemoryArticlesRepository,
+):
+    articles_repository.storage = {article}
+
+    article_from_service = await article_service.get_by_oid(article.oid)
+
+    assert article_from_service == article
